5f5ef2811343750c0f9177f3f1d8a8a8e0dac346
Bernardo Cezar <b.cezar1812@gmail.com>
2024-07-17 17:32:14 -0300

    added URL connection

--- db.js
+++ db.js
@@ -1,5 +1,17 @@
-// db.js
+import "dotenv/config";
 import postgres from "postgres";
 
-const sql = postgres({});
-export default sql;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+PGPASSWORD = decodeURIComponent(PGPASSWORD);
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
